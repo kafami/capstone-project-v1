5fd0d509a71c485f3c486e66edb7237f447d1f8d
kafami <64363760+kafami@users.noreply.github.com>
2024-04-25 13:49:00 +0700

    styling navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
 <div class="navbar">
     <div class="logo">
@@ -10,13 +11,31 @@
     </div>
     <div class="navbarMenu">
         <div class="navbarMenuOpt">
-            <a href="/"><p>Room Information</p></a>
+            <img class="navbar-icon-image" src="{{ asset('img/iconsRoomInformation.svg') }}" alt="">
+            <a class="navbar-icon" href="/"><p>Room Information</p></a>
         </div>
-        <div class="navbarMenuOpt">
+
+        <span class="fa-stack">
+            <i class="far fa-calendar fa-stack-2x"></i>
+            <strong class="fa-stack-1x calendar-text" id="date"></strong>
+        </span>
+        
+        <div class="calendarOpt">
             <a href=""><p>Calendar</p></a>
         </div>
+
         <div class="navbarMenuOpt">
-            <a href="/login"><p>Login</p></a>
+            <img class="navbar-icon-image" src="{{ asset('img/iconsLogin.svg') }}" alt="">
+            <a class="navbar-icon" href="/login"><p>Login</p></a>
         </div>
     </div>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+    // When the document is ready
+    $(document).ready(function() {
+        // Update date, month, and year
+        $('#date').text(new Date().getDate());
+    });
+</script>
